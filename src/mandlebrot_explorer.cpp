7e24da6a95d4b8ccd42c59b7f9c4729e6234fbc1
Amal <jshakes213@gmail.com>
2020-04-20 22:32:29 -0400

    change to use OpenMP

--- src/mandlebrot_explorer.cpp
+++ src/mandlebrot_explorer.cpp
@@ -14,53 +14,6 @@
 
 static const std::string program_name = "Mandlebrot Explorer";
 
-// Take in as conditions what part of the fractal to calculate,
-// then dump it into its alotted segment of &iterations
-void calculate_iterations(const int i_start,    const int i_end,
-                          const int j_start,    const int j_end,
-                          const int pixelWidth, const int order, const size_t nIter,
-                          const double x_min,   const double y_max, const double x_inc, const double y_inc,
-                          std::vector<double> &iterations)
-{
-    for (int i = i_start; i < i_end; i++)
-    {
-        for(int j = j_start; j < j_end; j++)
-        {
-            const std::complex<double> currentPoint(x_min+(j-j_start)*x_inc, y_max-(i-i_start)*y_inc);
-            size_t k = 0;
-            // cardiod improvement for 2nd order
-            if (order == 2)
-            {
-                const double q = (currentPoint.real() - 1.0/4.0)* ( currentPoint.real() - 1.0/4.0)
-                                + currentPoint.imag() * currentPoint.imag();
-                if (4 * q*(q+(currentPoint.real() - 1.0/4.0)) <= currentPoint.imag() * currentPoint.imag())
-                {
-                    k = nIter;
-                }
-            }
-
-            std::complex<double> cp_iterate(currentPoint);
-            while(k < nIter && abs(cp_iterate) < mandlebrot::BAILOUT_RADIUS)
-            {
-                cp_iterate = std::pow(cp_iterate, order) + currentPoint;
-                k += 1;
-            }
-
-            if (k == nIter)
-            {
-                iterations[i * pixelWidth + j] = k;
-            }
-            // compute fractional iterations
-            else
-            {
-                const double log_zn = log(abs(cp_iterate));
-                const double nu     = log(log_zn/log(mandlebrot::BAILOUT_RADIUS))/log(order);
-                iterations[i * pixelWidth + j] = k + 1 - nu;
-            }
-        }
-    }
-}
-
 int main()
 {
 
@@ -132,33 +85,44 @@ int main()
             const double y_inc = static_cast <double>(y_width) / mandlebrot::pixelWidth;
             const double x_inc = static_cast <double>(x_width) / mandlebrot::pixelWidth;
 
-            // calculate iterations for the new mandlebrot
-            // send off threads to calculate iterations for each slice of the fractal and then return back
-            std::vector<std::thread> th;
-            th.reserve(mandlebrot::NUM_THREADS);
-            for (int i = 0; i < mandlebrot::NUM_THREADS; i++)
-            {
-                th.push_back(std::thread(calculate_iterations,
-                                         i * mandlebrot::pixelWidth / mandlebrot::NUM_THREADS,       // i start
-                                         (i + 1) * mandlebrot::pixelWidth / mandlebrot::NUM_THREADS, // i end
-                                         0, // j start
-                                         mandlebrot::pixelWidth, // j end
-                                         mandlebrot::pixelWidth, // num_pixels
-                                         order, // order
-                                         nIter, //iter
-                                         x_min, // xmin
-                                         y_max - (i*y_width)/mandlebrot::NUM_THREADS, // y max
-                                         x_inc, // x increment
-                                         y_inc, // y increment
-                                         std::ref(iterations)) // iterations vector
-                );
-            }
-
             SDL_SetWindowTitle(window, (std::string("~ ") + program_name + std::string( ",   calculating ~") ).c_str());
             SDL_PumpEvents();
 
-            std::for_each(th.begin(), th.end(), [](auto &t) { t.join(); });
-
+            // calculate iterations for the new mandlebrot
+            // send off threads to calculate iterations for each slice of the fractal and then return back
+            #pragma omp parallel for
+            for(int i = 0; i < mandlebrot::pixelWidth; i++)
+            {
+                for (int j = 0; j < mandlebrot::pixelWidth; j++)
+                {
+                    const auto cp = std::complex<double>(x_min+j*x_inc, y_max-i*y_inc);
+                    size_t k = 0;
+                    // cardiod improvement for 2nd order
+                    if (order == 2)
+                    {
+                        const double q = (cp.real() - 1.0/4.0)* ( cp.real() - 1.0/4.0)
+                                        + cp.imag() * cp.imag();
+                        if (4 * q*(q+(cp.real() - 1.0/4.0)) <= cp.imag() * cp.imag())
+                        {
+                            k = nIter;
+                        }
+                    }
+                    std::complex<double> cp_iterate(cp);
+                    while(k < nIter && abs(cp_iterate) < mandlebrot::BAILOUT_RADIUS)
+                    {
+                        cp_iterate = std::pow(cp_iterate, order) + cp;
+                        k += 1;
+                    }
+                    if (k == nIter)
+                    {
+                        iterations[i*mandlebrot::pixelWidth+j] = k;
+                        continue;
+                    }
+                    const double log_zn = std::log(std::abs(cp_iterate));
+                    const double nu     = (std::log(log_zn)-std::log(mandlebrot::BAILOUT_RADIUS))/std::log(order);
+                    iterations[i*mandlebrot::pixelWidth+j] = k + 1 - nu;
+                }
+            }
             recalculate = false;
             redraw = true;
         }
@@ -241,9 +205,9 @@ int main()
                         [] (const auto &cc)
                         {
                             std::cout << "    "
-                                      << "{0x"  << std::setw(2) << static_cast<int>(cc[0])
-                                      << ", 0x" << std::setw(2) << static_cast<int>(cc[1])
-                                      << ", 0x" << std::setw(2) << static_cast<int>(cc[2]) << "}," << "\n";
+                                      << "{0x"  << std::setw(2) << static_cast<int>(cc[mandlebrot::RED])
+                                      << ", 0x" << std::setw(2) << static_cast<int>(cc[mandlebrot::BLUE])
+                                      << ", 0x" << std::setw(2) << static_cast<int>(cc[mandlebrot::GREEN]) << "}," << "\n";
                         });
                         std::cout << "};" << "\n"
                                   << std::dec << std::setfill(' ');
--- src/rendering.cpp
+++ src/rendering.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <vector>
 
+#include <iostream>
+
 #include "config.h"
 #include "rendering.h"
 
@@ -9,46 +11,48 @@
 //similar iterations will be similar color and we will shade the difference
 //this way, in theory each 1/NUM_BUCKETS group will have a similar color
 //In theory, each Bucket should then have TOTAL_PIXELS/NUM_BUCKETS in it
-void mandlebrot::histogram_render ( const std::vector< std::vector <unsigned char> > &current_colors,
+void mandlebrot::histogram_render (const std::vector< std::vector <unsigned char> > &current_colors,
                                     const std::vector<double> &iterations, int nIter,
-                                    SDL_Renderer *renderer )
+                                    SDL_Renderer *renderer)
 {
 
     std::vector<double> temp(iterations);
+    std::sort(temp.begin(), temp.end());
+    const auto nIterIt = std::find(temp.begin(), temp.end(), nIter);
+    const size_t nIterIdx = nIterIt - temp.begin() - (nIterIt == temp.end() ? 1 : 0);
 
-    std::sort( temp.begin(), temp.end() );
-
-    auto nIterIndex = std::find( temp.begin(), temp.end(), nIter );
-
-    std::vector<double>buckets( current_colors.size() );
-    for ( size_t i = 0; i < current_colors.size(); i++ )
+    std::vector<double>buckets(current_colors.size());
+    for (size_t i = 0; i < current_colors.size(); i++)
     {
-        buckets[ i ] = temp[ ( ( ( nIterIndex - temp.begin() ) * ( i + 1 ) ) / current_colors.size() ) - 1 ];
+        size_t calc_idx = (nIterIdx*(i+1))/(current_colors.size()) - 1;
+        calc_idx = calc_idx + 1 == 0 ? 0 : calc_idx;
+        buckets[i] = temp[calc_idx];
     }
 
     //ensure no 2 buckets have the same iteration count
     //this is very rudimentary and should be improved for smoother coloring
-    for ( size_t i = 0; i < current_colors.size() - 1; i++ )
+    for (size_t i = 0; i < current_colors.size() - 1; i++)
     {
-        if ( buckets[ i ] == buckets[ i + 1 ] )
+        if (buckets[i] == buckets[i + 1])
         {
-            if ( i == 0 )
+            if (i == 0)
             {
-                buckets[ i ] *= 0.2;
+                buckets[i] *= 0.2;
             }
             else
             {
-                buckets[ i ] = buckets[ i - 1 ] + 1 + 0.2 * ( buckets[ i ] - buckets[ i - 1 ] );
+                buckets[i] = buckets[i - 1] + 1 + 0.2 * (buckets[i] - buckets[i - 1]);
             }
         }
     }
 
-    for ( int i = 0; i < mandlebrot::pixelWidth; i++ )
+    #pragma omp parallel for
+    for (int i = 0; i < mandlebrot::pixelWidth; i++)
     {
-        for ( int j = 0; j < mandlebrot::pixelWidth; j++ )
+        for (int j = 0; j < mandlebrot::pixelWidth; j++)
         {
             char red, green, blue;
-            if ( iterations[ i * mandlebrot::pixelWidth + j ] == nIter )
+            if (iterations[i * mandlebrot::pixelWidth + j] == nIter)
             {
                 red   = 0x00;
                 green = 0x00;
@@ -59,42 +63,46 @@ void mandlebrot::histogram_render ( const std::vector< std::vector <unsigned cha
 
                 //find which bucket each pixel belongs to
                 size_t bucket_index, bucket2_index;
-                for ( bucket_index = 0; bucket_index < current_colors.size(); bucket_index++ )
+                for (bucket_index = 0; bucket_index < buckets.size() - 1; bucket_index++)
                 {
-                    if ( iterations[ i * mandlebrot::pixelWidth + j ] <= buckets[ bucket_index ] )
+                    if (iterations[i * mandlebrot::pixelWidth + j] <= buckets[bucket_index])
                         break;
                 }
 
                 bucket2_index = bucket_index == 0 ? buckets.size() -1 : bucket_index - 1;
 
-                double blend = static_cast<double>( iterations[ i * mandlebrot::pixelWidth + j ] - buckets[ bucket_index - 1 ] )
-                        / static_cast<double>( buckets[ bucket_index ] - buckets[ bucket_index - 1 ] );
+                double blend = static_cast<double>(iterations[i * mandlebrot::pixelWidth + j] - buckets[bucket_index - 1])
+                        / static_cast<double>(buckets[bucket_index] - buckets[bucket_index - 1]);
 
-                red   = static_cast<char>( ( 1 - blend ) * current_colors[ bucket2_index ][ mandlebrot::RED   ]
-                        + blend * current_colors[ bucket_index ][ mandlebrot::RED   ] );
-                green = static_cast<char>( ( 1 - blend ) * current_colors[ bucket2_index ][ mandlebrot::GREEN ]
-                        + blend * current_colors[ bucket_index ][ mandlebrot::GREEN ] );
-                blue  = static_cast<char>( ( 1 - blend ) * current_colors[ bucket2_index ][ mandlebrot::BLUE  ]
-                        + blend * current_colors[ bucket_index ][ mandlebrot::BLUE  ] );
+                red   = static_cast<char>((1 - blend) * current_colors[bucket2_index][mandlebrot::RED]
+                        + blend * current_colors[bucket_index][mandlebrot::RED]);
+                green = static_cast<char>((1 - blend) * current_colors[bucket2_index][mandlebrot::GREEN]
+                        + blend * current_colors[bucket_index][mandlebrot::GREEN]);
+                blue  = static_cast<char>((1 - blend) * current_colors[bucket2_index][mandlebrot::BLUE]
+                        + blend * current_colors[bucket_index][mandlebrot::BLUE]);
             }
-            SDL_SetRenderDrawColor( renderer, red, green, blue, 0xFF );
-            SDL_RenderDrawPoint   (renderer, j, i);
+            #pragma omp critical
+            {
+                SDL_SetRenderDrawColor(renderer, red, green, blue, 0xFF);
+                SDL_RenderDrawPoint   (renderer, j, i);
+            }
         }
     }
 }
 
 //color each pixel by the modulo of the iterations it took
 //this has the advantage of being zoom invariant, but can get messy
-void mandlebrot::modulo_render ( const std::vector< std::vector <unsigned char> > &current_colors,
+void mandlebrot::modulo_render (const std::vector< std::vector <unsigned char> > &current_colors,
                                  const std::vector<double> &iterations, int nIter, double modulo_blend,
-                                 SDL_Renderer *renderer )
+                                 SDL_Renderer *renderer)
 {
-    for ( int i = 0; i < mandlebrot::pixelWidth; i++ )
+    #pragma omp parallel for
+    for (int i = 0; i < mandlebrot::pixelWidth; i++)
     {
-        for ( int j = 0; j < mandlebrot::pixelWidth; j++ )
+        for (int j = 0; j < mandlebrot::pixelWidth; j++)
         {
             char red, green, blue;
-            if ( iterations[ i * mandlebrot::pixelWidth + j ] == nIter )
+            if (iterations[i * mandlebrot::pixelWidth + j] == nIter)
             {
                 red   = 0x00;
                 green = 0x00;
@@ -103,32 +111,35 @@ void mandlebrot::modulo_render ( const std::vector< std::vector <unsigned char> 
             else
             {
 
-                int bucket_index  = static_cast<int>( std::floor( iterations[ i * mandlebrot::pixelWidth + j ]
-                                    / modulo_blend ) ) % current_colors.size();
+                int bucket_index  = static_cast<int>(std::floor(iterations[i * mandlebrot::pixelWidth + j]
+                                    / modulo_blend)) % current_colors.size();
 
-                int bucket2_index = ( bucket_index + 1 ) % current_colors.size();
+                int bucket2_index = (bucket_index + 1) % current_colors.size();
 
-                double tmp = iterations[ i * mandlebrot::pixelWidth + j ];
+                double tmp = iterations[i * mandlebrot::pixelWidth + j];
                 double tmp2;
 
-                for ( tmp2 = 0; tmp2 < tmp - modulo_blend; tmp2 += modulo_blend )
+                for (tmp2 = 0; tmp2 < tmp - modulo_blend; tmp2 += modulo_blend)
                 {
                     ;
                 }
 
-                double blend = static_cast<double>( iterations[ i * mandlebrot::pixelWidth + j ] - tmp2 ) / modulo_blend;
+                double blend = static_cast<double>(iterations[i * mandlebrot::pixelWidth + j] - tmp2) / modulo_blend;
 
-                red   = static_cast<char>( ( 1 - blend ) * current_colors[ bucket_index ][ mandlebrot::RED   ]
-                      + blend * current_colors[ bucket2_index ][ mandlebrot::RED   ] );
+                red   = static_cast<char>((1 - blend) * current_colors[bucket_index][mandlebrot::RED]
+                      + blend * current_colors[bucket2_index][mandlebrot::RED]);
 
-                green = static_cast<char>( ( 1 - blend ) * current_colors[ bucket_index ][ mandlebrot::GREEN ]
-                      + blend * current_colors[ bucket2_index ][ mandlebrot::GREEN ] );
+                green = static_cast<char>((1 - blend) * current_colors[bucket_index][mandlebrot::GREEN]
+                      + blend * current_colors[bucket2_index][mandlebrot::GREEN]);
 
-                blue  = static_cast<char>( ( 1 - blend ) * current_colors[ bucket_index ][ mandlebrot::BLUE  ]
-                      + blend * current_colors[ bucket2_index ][ mandlebrot::BLUE  ] );
+                blue  = static_cast<char>((1 - blend) * current_colors[bucket_index][mandlebrot::BLUE]
+                      + blend * current_colors[bucket2_index][mandlebrot::BLUE]);
             }
-            SDL_SetRenderDrawColor( renderer, red, green, blue, 0xFF );
-            SDL_RenderDrawPoint   (renderer, j, i);
+            #pragma omp critical
+            {
+                SDL_SetRenderDrawColor(renderer, red, green, blue, 0xFF);
+                SDL_RenderDrawPoint   (renderer, j, i);
+            }
         }
     }
 }
