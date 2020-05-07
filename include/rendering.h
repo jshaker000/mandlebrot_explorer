473e187bec8be7d9b52d317c52c6892969fde43a
Joseph Shaker <jshakes213@gmail.com>
2020-05-07 15:37:53 -0400

    fixed header path

--- include/rendering.h
+++ include/rendering.h
@@ -1,7 +1,7 @@
 #ifndef  RENDERING_H
 #define RENDERING_H
 
-#include <SDL2/SDL.h>
+#include "SDL.h"
 #include <vector>
 
 namespace mandlebrot
@@ -9,7 +9,7 @@ namespace mandlebrot
     void histogram_render ( const std::vector< std::vector <unsigned char> > &current_colors, 
                             const std::vector<double> &iterations, int nIter, 
                             SDL_Renderer *renderer );
-    
+
     void modulo_render    ( const std::vector< std::vector <unsigned char> > &current_colors,
                             const std::vector<double> &iterations, int nIter, double modulo_blend, 
                             SDL_Renderer *renderer );
